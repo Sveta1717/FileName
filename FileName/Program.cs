322084e244b055f34441b2ddee36526a5b3ebbba
PC <PC@DESKTOP-441UH1F>
2022-07-09 23:55:00 +0300

    Add project files.

---
+++ FileName/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FileName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Показати шдях до директорії");
+
+
+
+            string text = File.ReadAllText(Console.ReadLine(), Encoding.GetEncoding(1251));
+
+            Console.WriteLine("Введить слово яке щукаете");
+            string pattern = Console.ReadLine();
+            int i = 0;
+            foreach (Match m in Regex.Matches(text, pattern, RegexOptions.IgnoreCase))
+                i += 1;
+            Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+    }
+}
